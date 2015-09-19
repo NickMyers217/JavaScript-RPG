ea6fb4dcade567595f68b6378cce26d061d5d2f4
nickmyers217 <nickbmyers217@gmail.com>
2015-09-18 23:00:29 -0700

    Started work on an entity system.

---
+++ js/entities/entity.js
@@ -0,0 +1,20 @@
+/*
+ * Entity module
+ *
+ */
+
+
+// Dependencies
+var sprite = require('./sprites.js');
+
+
+function Entity (x, y, sprite) {
+	this.x = x;
+	this.y = y;
+	this.sprite = sprite;
+}
+
+
+exports.createEntity = function (x, y, sprite) {
+	return new Entity(x, y, sprite);
+};
